bac0436d4e8c82c455159ac4d635743553d259ac
Ariadna Afanaskina <afanask1nanyc@gmail.com>
2022-04-26 20:20:03 +0700

    Подключение к бд

--- intern-track-back/Program.cs
+++ intern-track-back/Program.cs
@@ -1,6 +1,15 @@
+using intern_track_back.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//Backend old school legacy
+//builder.Services.AddMvc();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
