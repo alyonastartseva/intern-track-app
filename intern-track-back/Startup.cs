758a68786db3847a218a4c658c7b7253f74881d6
Tanya-Kleynos <>
2022-04-26 20:08:30 +0700

    ApplicationDbContext.cs

---
+++ intern-track-back/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using intern_track_back.Data;
+
+namespace intern_track_back
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
+        }
+    }
+}
