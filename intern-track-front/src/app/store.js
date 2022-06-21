82391459eaafc3b6bbef6aad0ad8741efd5e5b7f
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-22 02:22:45 +0700

    Добавлено частичное API для регистрация, вёрстка главной страницы (#17)

--- intern-track-front/src/app/store.js
+++ intern-track-front/src/app/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { authApi } from './store/api/auth';
+
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer
+  },
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
 });
