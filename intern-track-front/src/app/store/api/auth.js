b18d670e11815bc292eb5d40ff948ad9597f5520
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 04:41:59 +0700

    ITA-458 - Вёрстка списков вакансий (#23)
    * Added work with vacancies list
    
    * Added layouts for the vacancies

--- intern-track-front/src/app/store/api/auth.js
+++ intern-track-front/src/app/store/api/auth.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:7049/api/Account/', mode: 'cors' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:44346/api/Account/', mode: 'cors' }),
   endpoints: (builder) => ({
     registerAsStudent: builder.mutation({
       query: (formData) => ({
