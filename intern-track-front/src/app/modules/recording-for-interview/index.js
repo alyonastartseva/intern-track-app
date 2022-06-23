95c98fa7b4768e39826859db264f08233d3c5096
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 02:04:15 +0700

    Added layout for company list (#22)

--- intern-track-front/src/app/modules/recording-for-interview/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { Card } from 'antd';
+import { Card, Spin, Result, Col, Row } from 'antd';
 
 import { useGetAllCompaniesQuery } from 'src/app/store/api/companies';
+
+import './RecordingForInterview.css';
 
 export const RecordingForInterview = () => {
   const { data, error, isLoading } = useGetAllCompaniesQuery();
 
-  console.log(data);
-
   return (
-    <Card title="НТР" extra={<Link to="/vacancies-and-recording">Вакансии</Link>} style={{ width: 300 }}>
-      <p>Описание</p>
-    </Card>
+    <div className="recordingPage">
+      <h1>Компании-партнёры</h1>
+      {isLoading ? (
+        <Spin className="loader" />
+      ) : error ? (
+        <Result status="500" title="Что-то пошло не так" subTitle="Не удалось загрузить список компаний" />
+      ) : (
+        <Row gutter={[16, 16]}>
+          {data.companies.map((company) => (
+            <Col key={company.companyId} span={8}>
+              <Card
+                title={company.name}
+                extra={<Link to={`/vacancies-and-recording/${company.companyId}`}>Посмотреть вакансии</Link>}
+              >
+                <p>{company.about}</p>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </div>
   );
 };
