b5793c61960aeb11473729512b598e2c3859b70a
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 04:41:59 +0700

    ITA-458 - Вёрстка списков вакансий (#23)
    * Added work with vacancies list
    
    * Added layouts for the vacancies

--- intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { ReactComponent as BackIcon } from 'src/assets/svg/backIcon.svg';
+
+import { Button, Col, Card, Divider } from 'antd';
+
+import './VacanciesAndRecording.css';
 
 export const VacanciesAndRecording = () => {
+  const { companyId } = useParams();
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>Vacancies and recording</p>
+    <div className="vacanciesPage">
+      <header>
+        <Button className="ita-btn _text _back" onClick={() => navigate(-1)}>
+          <BackIcon />
+          Назад к компаниям
+        </Button>
+        <h2>Доступные вакансии</h2>
+      </header>
+      <Col className="vacancies" span={8}>
+        <Card title="Фронтенд разработчик">
+          <p>
+            <span className="descTitle">Описание:</span> Нужен хороший фронт
+          </p>
+          <p>
+            <span className="descTitle">Количество мест:</span> 2
+          </p>
+        </Card>
+      </Col>
+      <Divider orientation="left">
+        <h2 className="recordingTitle">Записаться на собеседование</h2>
+      </Divider>
     </div>
   );
 };
