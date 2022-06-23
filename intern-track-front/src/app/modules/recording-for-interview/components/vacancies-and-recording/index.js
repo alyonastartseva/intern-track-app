3210f23fd4fcd25392fab9fc13ff50f262587489
Alyona <alyona_startseva.tk@mail.ru>
2022-06-24 02:55:24 +0700

    Added work with vacancies list

--- intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { ReactComponent as BackIcon } from 'src/assets/svg/backIcon.svg';
+
+import { Button } from 'antd';
+
+import './VacanciesAndRecording.css';
 
 export const VacanciesAndRecording = () => {
+  const { companyId } = useParams();
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>Vacancies and recording</p>
+    <div className="vacanciesPage">
+      <Button className="ita-btn _text _back" onClick={() => navigate(-1)}>
+        <BackIcon />
+        Назад к компаниям
+      </Button>
+      <p>{companyId}</p>
     </div>
   );
 };
