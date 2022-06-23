e2fc5438eae8d04f63a47ceeac0e8a55175b52d9
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 01:13:15 +0700

    Added component for companies (#20)

--- intern-track-front/src/app/store.js
+++ intern-track-front/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { authApi } from './store/api/auth';
+import { companiesApi } from './store/api/companies';
 
 export const store = configureStore({
   reducer: {
-    [authApi.reducerPath]: authApi.reducer
+    [authApi.reducerPath]: authApi.reducer,
+    [companiesApi.reducerPath]: companiesApi.reducer
   },
 
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware, companiesApi.middleware)
 });
