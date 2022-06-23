28df5a2cd124be7ae1b1988570d5861d8efc96ee
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 04:41:59 +0700

    ITA-458 - Вёрстка списков вакансий (#23)
    * Added work with vacancies list
    
    * Added layouts for the vacancies

--- intern-track-front/src/app/store/api/companies.js
+++ intern-track-front/src/app/store/api/companies.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const companiesApi = createApi({
   reducerPath: 'companiesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:7049/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:44346/api/' }),
   endpoints: (builder) => ({
     getAllCompanies: builder.query({
       query: () => 'User/getallcompanies'
