c65fe5714560697917a7bcaa7aad3b56150e4318
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 01:13:15 +0700

    Added component for companies (#20)

--- intern-track-front/src/app/modules/recording-for-interview/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
+
+import { useGetAllCompaniesQuery } from 'src/app/store/api/companies';
+
 export const RecordingForInterview = () => {
+  const { data, error, isLoading } = useGetAllCompaniesQuery();
+
+  console.log(data);
+
   return (
-    <div>
-      <p>Запись на собеседование</p>
-    </div>
+    <Card title="НТР" extra={<Link to="/vacancies-and-recording">Вакансии</Link>} style={{ width: 300 }}>
+      <p>Описание</p>
+    </Card>
   );
 };
