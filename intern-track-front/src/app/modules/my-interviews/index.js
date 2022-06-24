3e29dcc3eda4bf398ee77df7d5dffcd4ea947336
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 13:45:54 +0700

    Добавлена вёрстка для страницы мои собеседования для студента (#30)

--- intern-track-front/src/app/modules/my-interviews/index.js
+++ intern-track-front/src/app/modules/my-interviews/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Table } from 'antd';
+
+import { columnsMyInterviews } from './const';
+
+import './MyInterviews.css';
+
 export const MyInterviews = () => {
   return (
-    <div>
-      <p>Мои собеседования</p>
+    <div className="myInterviewsPage">
+      <h1>Мои собеседования</h1>
+      <Table columns={columnsMyInterviews} dataSource={[]} />
     </div>
   );
 };
