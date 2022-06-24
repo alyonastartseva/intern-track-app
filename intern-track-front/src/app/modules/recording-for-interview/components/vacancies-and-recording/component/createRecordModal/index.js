806fa4590730be32df3b27f297ea5f62ba4b4d0f
Alyona <alyona_startseva.tk@mail.ru>
2022-06-24 14:22:30 +0700

    Добавлено получение доступных стэков исходя из доступных вакансий

--- intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/component/createRecordModal/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/component/createRecordModal/index.js
@@ -1,12 +1,18 @@
 import React, { useCallback } from 'react';
 
+import { useParams } from 'react-router-dom';
 import { Modal, Form, Input, Select } from 'antd';
 
-import { priorityDict, stackTypesDict } from '../../const';
+import { priorityDict } from '../../const';
+import { useGetStackTypesByCompanyIdQuery } from 'src/app/store/api/record';
 
 const { Option } = Select;
 
 export const CreateRecordModal = ({ isVisible, onCancel, onOk }) => {
+  const { companyId } = useParams();
+
+  const { data: stackTypes } = useGetStackTypesByCompanyIdQuery(companyId || '');
+
   const [form] = Form.useForm();
 
   const handleAfterClose = useCallback(() => {
@@ -39,7 +45,7 @@ export const CreateRecordModal = ({ isVisible, onCancel, onOk }) => {
         </Form.Item>
         <Form.Item name="stackTypes" label="Желаемые направления стажировки" rules={[{ required: true }]}>
           <Select mode="multiple">
-            {stackTypesDict.map((item) => (
+            {stackTypes?.map((item) => (
               <Option key={item.key} value={item.key}>
                 {item.value}
               </Option>
