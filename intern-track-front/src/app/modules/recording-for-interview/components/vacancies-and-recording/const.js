583996ef2dc3f824d6e51906443dc1982f523232
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 11:11:28 +0700

    ITA-480 - Вёрстка страницы записи на собеседования для студента (#26)
    * Добавлена вёрстка записи на собеседование
    
    * Правки

---
+++ intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/const.js
@@ -0,0 +1,48 @@
+export const columnsRecording = [
+  {
+    title: 'Студент',
+    dataIndex: 'studentName',
+    key: 'studentName'
+  },
+  {
+    title: 'Предпочитаемое время',
+    dataIndex: 'preferableTime',
+    key: 'preferableTime'
+  },
+  {
+    title: 'Желаемый стэк',
+    dataIndex: 'stackTypes',
+    key: 'stackTypes'
+  },
+  {
+    title: 'Приоритет',
+    dataIndex: 'priority',
+    key: 'priority'
+  }
+];
+
+export const priorityDict = [
+  {
+    value: 'Высокий',
+    key: 1
+  },
+  {
+    value: 'Средний',
+    key: 2
+  },
+  {
+    value: 'Низкий',
+    key: 3
+  }
+];
+
+export const stackTypesDict = [
+  {
+    value: 'Frontend',
+    key: 'frontend'
+  },
+  {
+    value: 'Backend',
+    key: 'backend'
+  }
+];
--- intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
+++ intern-track-front/src/app/modules/recording-for-interview/components/vacancies-and-recording/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 import { ReactComponent as BackIcon } from 'src/assets/svg/backIcon.svg';
 import { useGetVacanciesByIdQuery } from 'src/app/store/api/companies';
+import { columnsRecording } from './const';
+import { CreateRecordModal } from './component/createRecordModal';
 
-import { Button, Col, Card, Divider } from 'antd';
+import { Button, Col, Card, Divider, Table } from 'antd';
 
 import './VacanciesAndRecording.css';
 
 export const VacanciesAndRecording = () => {
+  const [createRecordModalVisible, setCreateRecordModalVisible] = useState(false);
+
   const { companyId } = useParams();
 
   const { data, error, isLoading } = useGetVacanciesByIdQuery(companyId || '');
 
-  console.log(data);
-
   const navigate = useNavigate();
+
+  const handleOnClickAddRecord = useCallback(() => {
+    setCreateRecordModalVisible((prev) => !prev);
+  }, []);
+
+  const handleOnOkRecordCreate = useCallback((formData) => {
+    console.log(formData);
+    setCreateRecordModalVisible((prev) => !prev);
+  }, []);
+
+  const handleOnCancelRecordCreate = useCallback((formData) => {
+    console.log(formData);
+    setCreateRecordModalVisible((prev) => !prev);
+  }, []);
 
   return (
     <div className="vacanciesPage">
@@ -36,9 +52,19 @@ export const VacanciesAndRecording = () => {
           </p>
         </Card>
       </Col>
-      <Divider orientation="left">
+      <Divider orientation="center">
         <h2 className="recordingTitle">Записаться на собеседование</h2>
       </Divider>
+      <Button className="ita-btn add-record" onClick={handleOnClickAddRecord}>
+        Добавить запись
+      </Button>
+      <Table dataSource={[]} columns={columnsRecording} />
+
+      <CreateRecordModal
+        isVisible={createRecordModalVisible}
+        onOk={handleOnOkRecordCreate}
+        onCancel={handleOnCancelRecordCreate}
+      />
     </div>
   );
 };
