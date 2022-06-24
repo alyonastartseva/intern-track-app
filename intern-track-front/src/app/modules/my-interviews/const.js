72280ff926421750ba6b56b2298d499e952af12f
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-24 13:45:54 +0700

    Добавлена вёрстка для страницы мои собеседования для студента (#30)

---
+++ intern-track-front/src/app/modules/my-interviews/const.js
@@ -0,0 +1,38 @@
+export const formatDict = [
+  {
+    key: '1',
+    value: 'Online'
+  },
+  {
+    key: '2',
+    value: 'Offline'
+  }
+];
+
+export const columnsMyInterviews = [
+  {
+    title: 'Компания',
+    dataIndex: 'companyName',
+    key: 'companyName'
+  },
+  {
+    title: 'Позиция',
+    dataIndex: 'stack',
+    key: 'stack'
+  },
+  {
+    title: 'Дата',
+    dataIndex: 'date',
+    key: 'date'
+  },
+  {
+    title: 'Формат',
+    dataIndex: 'format',
+    key: 'format'
+  },
+  {
+    title: 'Место',
+    dataIndex: 'place',
+    key: 'place'
+  }
+];
