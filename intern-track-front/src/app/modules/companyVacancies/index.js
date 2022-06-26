e62772afaca21a06506a2f8d3469ce6e2e9991b7
Alyona <alyona_startseva.tk@mail.ru>
2022-06-26 18:38:01 +0700

    Добавлена карточка вакансии

--- intern-track-front/src/app/modules/companyVacancies/index.js
+++ intern-track-front/src/app/modules/companyVacancies/index.js
@@ -1,28 +1,56 @@
 import React, { useCallback, useState } from 'react';
 
-import { Button } from 'antd';
+import { Button, Card, Col, Row } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 import { CreateVacancyModal } from './components/createVacancyModal';
+import './CompanyVacancies.css';
 
 export const CompanyVacancies = () => {
   const [visibleCreateModal, setVisibleCreateModal] = useState(false);
 
-  const handleOnOk = useCallback((formData) => {
+  const handleOnOkCreateVacancy = useCallback((formData) => {
     console.log(formData);
     setVisibleCreateModal((prev) => !prev);
   }, []);
 
-  const handleOnCancel = useCallback(() => {
+  const handleOnCancelCreateVacancy = useCallback(() => {
     setVisibleCreateModal((prev) => !prev);
+  }, []);
+
+  const handleOnEditVacancy = useCallback((id) => {
+    console.log(id);
+  }, []);
+
+  const handleOnDeleteVacancy = useCallback((id) => {
+    console.log(id);
   }, []);
 
   return (
     <div>
-      <Button className="ita-btn" onClick={() => setVisibleCreateModal((prev) => !prev)}>
+      <Button className="ita-btn create-vacancy" onClick={() => setVisibleCreateModal((prev) => !prev)}>
         Создать вакансию
       </Button>
 
-      <CreateVacancyModal isVisible={visibleCreateModal} onOk={handleOnOk} onCancel={handleOnCancel} />
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Card
+            title="Название вакансии"
+            actions={[
+              <EditOutlined onClick={() => handleOnEditVacancy(5)} key="edit" />,
+              <DeleteOutlined onClick={() => handleOnDeleteVacancy(5)} key="delete" />
+            ]}
+          >
+            <p>Описание вакансии</p>
+          </Card>
+        </Col>
+      </Row>
+
+      <CreateVacancyModal
+        isVisible={visibleCreateModal}
+        onOk={handleOnOkCreateVacancy}
+        onCancel={handleOnCancelCreateVacancy}
+      />
     </div>
   );
 };
