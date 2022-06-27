d123249328782d4620fabe04edc3e65d9d3bf8a6
Ariadna Afanaskina <afanask1nanyc@gmail.com>
2022-06-28 05:54:05 +0700

    Ещё одно исправление получения списка студентов

--- intern-track-back/Enumerations/GeneralStudentStatusType.cs
+++ intern-track-back/Enumerations/GeneralStudentStatusType.cs
@@ -4,7 +4,7 @@ namespace intern_track_back.Enumerations
 {
     public enum GeneralStudentStatusType
     {
-        [Display(Name = "Студент ничего не делал")]
+        /*[Display(Name = "Студент ничего не делал")]
         DidNothing = 1,
         
         [Display(Name = "Студенту назначали собеседования")]
@@ -14,6 +14,20 @@ namespace intern_track_back.Enumerations
         ReceivedOffer = 3,
 
         [Display(Name = "Студент принял оффер")]
-        ConfirmedOffer = 4
+        ConfirmedOffer = 4*/
+        [Display(Name = "Собеседование ожидается")]
+        Waiting = 1,
+
+        [Display(Name = "Собеседование пройдено")]
+        Happened = 2,
+
+        [Display(Name = "Отказано")]
+        Denied = 3,
+
+        [Display(Name = "Предложение оффера")]
+        SendOffer = 4,
+
+        [Display(Name = "Оффер подтвержден")]
+        ConfirmOffer = 5
     }
 }
