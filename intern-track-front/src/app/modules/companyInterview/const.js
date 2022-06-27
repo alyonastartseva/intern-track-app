1e04d3e1f65cebd59611f2ef012263c72db88222
Alyona Startseva <alyona_startseva.tk@mail.ru>
2022-06-27 21:40:04 +0700

    Добавлен moment, вёрстка страница управления компанией вакансиями (#42)

---
+++ intern-track-front/src/app/modules/companyInterview/const.js
@@ -0,0 +1,22 @@
+export const studentInterviewsStatusType = [
+  {
+    key: '1',
+    value: 'Собеседование ожидается'
+  },
+  {
+    key: '2',
+    value: 'Собеседование пройдено'
+  },
+  {
+    key: '3',
+    value: 'Отказано'
+  },
+  {
+    key: '4',
+    value: 'Предложение оффера'
+  },
+  {
+    key: '5',
+    value: 'Оффер подтверждён'
+  }
+];
